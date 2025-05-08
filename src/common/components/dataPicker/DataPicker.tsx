b420f8fdf6ef3ce039b7885a25bb0ca742e22d37
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-05-08 17:00:13 +0300

    install library react-day-picker

---
+++ src/common/components/dataPicker/DataPicker.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
+import {useState} from "react";
+
+export function MyDatePicker() {
+    const [selected, setSelected] = useState<Date>();
+
+    return (
+        <DayPicker
+            animate
+            mode="single"
+            selected={selected}
+            onSelect={setSelected}
+            footer={
+                selected ? `Selected: ${selected.toLocaleDateString()}` : "Pick a day."
+            }
+        />
+    );
+}
