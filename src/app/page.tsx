0a141738e8591b472badf786d2ef6c4f971f86a1
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-05-17 10:27:36 +0300

    added header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,19 @@
 import Image from "next/image";
+import {Header} from "@/common/components/header/Header";
 
 export default function Home() {
   return (
     <div>
+      <header className="fixed top-0 w-screen bg-gray-100 flex items-center justify-center flex-wrap">
+        <Header />
+      </header>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
         <div className="w-screen min-h-screen bg-blue-500 text-white flex items-center justify-center text-3xl font-bold">
           Tailwind работает! 👌
         </div>
 
       </main>
-      <footer className="fixed bottom-0 w-screen bg-white flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="fixed bottom-0 w-screen bg-gray-100 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
---
+++ src/common/components/header/Header.tsx
@@ -0,0 +1,8 @@
+type Props = {};
+export const Header = (props: Props) => {
+    return (
+        <div >
+            <h1> my project!</h1>
+        </div>
+    );
+};
