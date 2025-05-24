c523031b9a13c61565f18a7ef227fbba31de4c2e
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-05-24 17:27:41 +0300

    added allocation pages

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import Link from "next/link";
 import { Locale } from "../../../../i18n-config";
 import { LanguageSwitcher } from "./LanguageSwitcher";
 import {ThemeSwitcher} from "@/common/components/ThemeSwitcher/ThemeSwitcher";
+import {usePathname} from "next/navigation";
 
 type Props = {
     lang: Locale;
@@ -20,24 +23,31 @@ type Props = {
 };
 
 export const Header = ({ lang, dictionary }: Props) => {
+    const pathname = usePathname();
+
+    // Список пунктов меню и их пути
+    const navLinks = [
+        { href: `/${lang}`, label: dictionary.header.home },
+        { href: `/${lang}/contact`, label: dictionary.header.contact },
+        { href: `/${lang}/cv`, label: dictionary.header.cv },
+        { href: `/${lang}/skills`, label: dictionary.header.skills ? dictionary.header.skills[0] : "Skills" },
+    ];
+
     return (
         <header className="fixed top-0 left-0 right-0 flex items-center justify-between px-4 py-2 w-full bg-gray-300">
             <Link href={`/${lang}`} className="text-xl font-bold">
                 {dictionary.general.projectName}
             </Link>
             <nav className="flex items-center gap-6 flex-1 justify-center">
-                <Link href={`/${lang}`} className="text-xl font-bold">
-                    {dictionary.header.home}
-                </Link>
-                <Link href={`/${lang}/contact`} className="flex items-center">
-                    {dictionary.header.contact}
-                </Link>
-                <Link href={`/${lang}/cv`} className="flex items-center mr-4">
-                    {dictionary.header.cv}
-                </Link>
-                <Link href={`/${lang}/skills`} className="flex items-center mr-4">
-                    {dictionary.header.skills ? dictionary.header.skills[0] : "Skills"}
-                </Link>
+                {navLinks.map(link => (
+                    <Link
+                        key={link.href}
+                        href={link.href}
+                        className={`flex items-center mr-4 ${pathname === link.href ? "font-bold" : ""}`}
+                    >
+                        {link.label}
+                    </Link>
+                ))}
             </nav>
             <ThemeSwitcher dict={dictionary.themeSwitcher} />
             <LanguageSwitcher currentLang={lang} />
