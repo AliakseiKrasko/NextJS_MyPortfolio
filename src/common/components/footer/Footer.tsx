a6bfe74d5c73e72c1dd665e0a36390d27f5356d2
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-07-12 20:10:30 +0300

    fixed bugs adaptiviti all project

--- src/common/components/footer/Footer.tsx
+++ src/common/components/footer/Footer.tsx
@@ -4,19 +4,10 @@ type Props = {};
 
 export const Footer = (props: Props) => {
     return (
-        <div className="fixed bottom-0 left-0 right-0 flex justify-center items-center w-full gap-5 transition-colors duration-300 bg-black/30 backdrop-blur">
-            <a className="flex items-center gap-2 hover:underline" href="https://nextjs.org/learn" target="_blank">
-                <Image src="/file.svg" alt="File icon" width={16} height={16} />
-                Learn
-            </a>
-            <a className="flex items-center gap-2 hover:underline" href="https://vercel.com/templates?framework=next.js" target="_blank">
-                <Image src="/window.svg" alt="Window icon" width={16} height={16} />
-                Examples
-            </a>
-            <a className="flex items-center gap-2 hover:underline" href="https://nextjs.org" target="_blank">
-                <Image src="/globe.svg" alt="Globe icon" width={16} height={16} />
-                Go to nextjs.org →
-            </a>
-        </div>
+        <footer className="w-full text-center text-gray-400 text-xs py-4 mt-16">
+            © {new Date().getFullYear()} Krasko Aliaksei |
+            <a href="mailto:kraskoaliaksei@gmail.com" className="ml-2 text-lime-400 hover:underline">Email me</a>
+            <a href="https://github.com/AliakseiKrasko" className="ml-2 text-lime-400 hover:underline" target="_blank">GitHub</a>
+        </footer>
     );
 };
