471fd8534e2a4fad33666ebe7185083168d41a3d
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-07-12 13:05:14 +0300

    Added responsive design for all devices.

--- src/feature/home/ui/HomeAnimatedView.tsx
+++ src/feature/home/ui/HomeAnimatedView.tsx
@@ -12,25 +12,22 @@ export default function HomeAnimatedView({
                                              common,
                                          }: HomeAnimatedViewProps) {
     return (
-        <div className={`min-h-screen relative overflow-hidden transition-all duration-700 ${show ? 'opacity-100' : 'opacity-0'}`}>
-            <div className="flex flex-col md:flex-row items-center justify-center gap-16 pt-8 max-w-5xl mx-auto">
-
-                <div className={`bg-white/5 border border-violet-600 rounded-2xl p-4 shadow-lg flex-grow-0 flex-shrink-0 basis-full md:basis-1/3 w-full md:w-1/3 home-border
-            transition-all duration-700 ${show ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-8'}`}>
+        <div className={`relative overflow-hidden transition-all duration-700 ${show ? 'opacity-100' : 'opacity-0'}`}>
+            <div className="flex flex-col md:flex-row items-center justify-center gap-8 pt-8 max-w-5xl mx-auto px-4">
+                <div className={`bg-white/5 border border-violet-600 rounded-2xl p-4 shadow-lg w-full max-w-[380px] md:max-w-[580px] mx-auto transition-all duration-700 ${show ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-8'}`}>
                     <Image
                         src="/my-photo2.jpg"
                         alt="Aliaksei Krasko"
                         width={580}
                         height={600}
-                        className="rounded-2xl object-cover w-[580px] h-[600px]"
+                        className="rounded-2xl object-cover w-full h-auto max-w-[380px] max-h-[450px] md:max-w-[580px] md:max-h-[600px]"
                     />
                 </div>
-
-                <div className={`w-full md:w-2/3 flex flex-col transition-all duration-700 ${show ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-8'}`}>
-                    <h1 className="text-violet-600 text-5xl font-extrabold mb-6 home-text">
+                <div className={`w-full md:w-2/3 flex flex-col px-4 md:px-8 transition-all duration-700 ${show ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-8'}`}>
+                    <h1 className="text-violet-600 text-4xl md:text-5xl font-extrabold mb-6 home-text">
                         Krasko Aliaksei
                     </h1>
-                    <h1 className="text-4xl font-bold mb-6 text-gray-600 cv-text">
+                    <h1 className="text-2xl md:text-4xl font-bold mb-6 text-gray-600 cv-text">
                         <Typewriter
                             words={typewriterWords}
                             loop={true}
@@ -42,8 +39,7 @@ export default function HomeAnimatedView({
                         />
                     </h1>
                     <p className="text-violet-600 cv-text text-lg mb-8">{description}</p>
-                    <div className="flex gap-6 mb-4">
-
+                    <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 mb-4 pb-6">
                         <Link
                             href={`/${lang}/contact`}
                             className="bg-[#b9ff39] text-black px-6 py-3 rounded-md font-bold flex items-center gap-2 shadow transition hover:bg-[#a2cb60]"
