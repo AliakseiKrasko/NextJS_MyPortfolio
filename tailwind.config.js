99d6555d020e2098b6d9e45a31c36a08374bc074
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-07-21 18:04:14 +0300

    fix next 15

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: 'class',
     content: [
         "./src/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./public/index.html"
     ],
-    theme: {
-        extend: {},
-    },
+    theme: { extend: {} },
     plugins: [],
 }
